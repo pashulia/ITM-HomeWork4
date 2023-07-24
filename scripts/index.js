617cc735f2fd046e9d1bfd2ce32fafb224b16702
Pavel Khegay <pashulia.pk@gmail.com>
2023-07-24 11:03:22 +0600

    add sass for 320px

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,13 @@ let exit = document.getElementById('exit');
 
 
 btnBurger.addEventListener('click', function(){
-    burger.classList.toggle('active');
+    burger.classList.remove('burger');
+    burger.classList.add('burger__active');
 })
 
 exit.addEventListener('click', function(){
-    burger.classList.toggle('active');
+    burger.classList.remove('burger__active');
+    burger.classList.add('burger');
 })
 
 // Получаем ссылки на элементы радио и меток
@@ -43,3 +45,94 @@ option3.addEventListener("change", function() {
         label3.textContent = "CH";
     }
 });
+
+const servicesList = document.querySelector('.services__list');
+        const servicesListCards = document.querySelectorAll('.services__list_card');
+        const servicesCardsList = document.querySelectorAll('.services__cards-item');
+        const servicesCards = document.querySelectorAll('.services__cards');
+        const showButton = document.querySelector('.show-button');
+        const hideButton = document.querySelector('.hide-button');
+        let cardsToShow = servicesListCards.length; // Изначально отображаем все элементы
+        let cardsToShow2 = servicesCardsList.length; // Изначально отображаем все элементы
+
+        // Функция для отображения определенного количества элементов
+        function displayCards() {
+            servicesListCards.forEach((card, index) => {
+                if (index < cardsToShow) {
+                    card.style.display = 'flex'; // Отображаем элемент
+                } else {
+                    card.style.display = 'none'; // Скрываем элемент
+                }
+            });
+        }
+
+        // Функция для отображения определенного количества элементов
+        function displayCards2() {
+            servicesCardsList.forEach((card, index) => {
+                if (index < cardsToShow2) {
+                    card.style.display = 'flex'; // Отображаем элемент
+                } else {
+                    card.style.display = 'none'; // Скрываем элемент
+                }
+            });
+        }
+
+        // Функция для отображения всех элементов
+        function showAllCards() {
+            cardsToShow = servicesListCards.length; // Показываем все элементы
+            displayCards();
+            showButton.style.display = 'none'; // Скрываем кнопку "Показать все"
+            hideButton.style.display = 'block'; // Показываем кнопку "Скрыть"
+        }
+
+        // Функция для скрытия лишних элементов
+        function hideExtraCards() {
+            if (window.matchMedia('(min-width: 768px)').matches) {
+                cardsToShow = 6; // Отображаем только 6 элементов по умолчанию
+            }
+            if (window.matchMedia('(min-width: 1120px)').matches) {
+                cardsToShow = 8; // Отображаем только 8 элементов по умолчанию
+            }
+            displayCards();
+            showButton.style.display = 'block'; // Показываем кнопку "Показать все"
+            hideButton.style.display = 'none'; // Скрываем кнопку "Скрыть"
+        }
+
+        // Вызываем функцию displayCards() при загрузке страницы
+        displayCards();
+
+        // Медиа-запрос для экранов шириной 320px
+        if (window.matchMedia('(min-width: 320px)').matches) {
+            cardsToShow = servicesListCards.length; // Отображаем все элементы по умолчанию
+            showButton.style.display = 'none'; // Скрываем кнопку "Показать все"
+            hideButton.style.display = 'none'; // Скрываем кнопку "Скрыть"
+            scrollCarousel(1); // Скроллим карусель на первый элемент
+        }
+        // Медиа-запрос для экранов шириной 768px
+        if (window.matchMedia('(min-width: 768px)').matches) {
+          cardsToShow = 6; // Отображаем только 6 элементов по умолчанию
+          hideExtraCards(); // Скрываем лишние элементы
+        }
+        // Медиа-запрос для экранов шириной 1120px
+        if (window.matchMedia('(min-width: 1120px)').matches) {
+          cardsToShow = 8; // Отображаем только 8 элементов по умолчанию
+          hideExtraCards(); // Скрываем лишние элементы
+        }
+
+        function scrollCarousel(index) {
+            const carousel = document.querySelector('.services__list');
+            let cardWidth = 240;
+            carousel.scrollLeft = (index - 1) * cardWidth;
+        }
+
+        function scrollCarousel2(index) {
+            const carousel = document.querySelector('.services__cards');
+            let cardWidth = 280;
+            carousel.scrollLeft = (index - 1) * cardWidth;
+        }
+
+        function scrollCarousel3(index) {
+            const carousel = document.querySelector('.services__price');
+            let cardWidth = 285;
+            carousel.scrollLeft = (index - 1) * cardWidth;
+        }
